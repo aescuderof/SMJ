dd4dde8bb3460453ba0428afdfc86338fdf6b7de
aescuderof <42484216+aescuderof@users.noreply.github.com>
2026-02-10 16:40:26 -0300

    config cors ok

--- src/index.js
+++ src/index.js
@@ -13,7 +13,25 @@ const app = express();
 
 connectDB();
 
-app.use(cors());
+const whiteList = [
+  'http:localhost:3000',
+]
+
+const corsOptions = {
+  origin: function(origin, callback) {
+    if (!origin) return callback(null, true);
+
+    if(whiteList.includes(origin)) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    },
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 
