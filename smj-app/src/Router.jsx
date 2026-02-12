8620597e2cd7848e86a512239254241b4d34a4a1
aescuderof <42484216+aescuderof@users.noreply.github.com>
2026-02-12 18:58:46 +0000

    footer

---
+++ smj-app/src/Router.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+
+const Router = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/*" element={<Layout />}>
+                    <Route index element={<Home />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default Router
